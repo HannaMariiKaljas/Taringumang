be207eacac33073a30096f4873f8db870485dad3
HannaMariiKaljas <hannamarii.kaljas@gmail.com>
2018-03-18 17:29:46 +0200

    Testklass

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Jou");
+        List<Integer> proov= new ArrayList<>();
+        proov.add(1);
+        proov.add(6);
+        proov.add(6);
+        proov.add(2);
+        proov.add(2);
+        proov.add(3);
+        Meetodid m1=new Meetodid(proov);
+        System.out.println(m1.Kolmik(proov));
     }
 }
