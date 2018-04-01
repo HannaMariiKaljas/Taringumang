2292c6ea5bd3900f3307cc53b9fb759302087f2b
HannaMariiKaljas <hannamarii.kaljas@gmail.com>
2018-04-01 17:06:40 +0300

    Testklass

--- src/Test.java
+++ src/Test.java
@@ -4,13 +4,7 @@ import java.util.List;
 public class Test {
     public static void main(String[] args) {
         List<Integer> proov= new ArrayList<>();
-        proov.add(1);
-        proov.add(6);
-        proov.add(6);
-        proov.add(2);
-        proov.add(2);
-        proov.add(3);
         Meetodid m1=new Meetodid(proov);
-        System.out.println(m1.kolmed(proov));
+        System.out.println(m1.ühed(proov));
     }
 }
