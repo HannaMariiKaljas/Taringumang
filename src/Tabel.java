e5a196368814ea71f6612334011147c61f9a6af9
HannaMariiKaljas <37441926+HannaMariiKaljas@users.noreply.github.com>
2018-04-03 14:58:44 +0300

    Update Tabel.java

--- src/Tabel.java
+++ src/Tabel.java
@@ -10,7 +10,28 @@ public class Tabel {
         }
         if (summa <= 63){
             summa -= 50;
+        }else {
+            summa+=50;
         }
         return summa;
+    }
+    public int alumineSumma(int[] veerg){
+        int summa = 0;
+        for (int i = 6; i < 14; i++) {
+            summa += veerg[i];
+        }
+        return summa;
+    }
+
+    public int josplidSumma( int[] josplid){
+        int summa=0;
+        for (int i = 0; i < 6; i++) {
+            summa+=josplid[i];
+        }
+        return summa;
+    }
+
+    public int üldineSumma (){
+        return alumineSumma(esimene)+alumineSumma(teine)+ülemineSumma(esimene)+ülemineSumma(teine)+josplidSumma(jospel);
     }
 }
