22b981640447953c47756190b78ba8c7d403ca2e
Viido Kaur Lutsar <37119942+Viido@users.noreply.github.com>
2018-05-07 22:01:21 +0300

    algne

--- src/EsimeneAken.java
+++ src/EsimeneAken.java
@@ -7,6 +7,8 @@ import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
@@ -14,7 +16,11 @@ import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 import javafx.stage.Window;
 
+import java.io.File;
+
 public class EsimeneAken extends Application {
+    public String mängija1;
+    public String mängija2;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -85,12 +91,16 @@ public class EsimeneAken extends Application {
                 if(nimiesimene.getText().isEmpty()) {
                     Veateade(Alert.AlertType.ERROR, gridPane.getScene().getWindow(), "Viga!", "Sisesta esimese mängija nimi");
                     return;
+                }else{
+                    mängija1 = nimiesimene.getText();
                 }
                 if(nimiteine.getText().isEmpty()) {
                     Veateade(Alert.AlertType.ERROR, gridPane.getScene().getWindow(), "Viga!", "Sisesta teise mängija nimi");
                     return;
+                }else{
+                    mängija2 = nimiteine.getText();
                 }
-                GridPane juur=new GridPane();
+                GridPane juur = mänguAken();
                 Stage stage=new Stage();
                 stage.setTitle("Täringumäng");
                 stage.setScene(new Scene(juur, 600,600));
@@ -100,6 +110,37 @@ public class EsimeneAken extends Application {
         });
     }
 
+    private GridPane mänguAken(){
+        GridPane gridpane = new GridPane();
+        gridpane.setPadding(new Insets(10,10,10,10));
+        gridpane.setVgap(50);
+        gridpane.setHgap(50);
+
+        TableView tableView = new TableView();
+
+        TableColumn reegel = new TableColumn("Reegel");
+        TableColumn m1 = new TableColumn(mängija1);
+        TableColumn m2 = new TableColumn(mängija2);
+
+        TableColumn m1esimene = new TableColumn("Esimene");
+        TableColumn m1teine = new TableColumn("Teine");
+        TableColumn m2esimene = new TableColumn("Esimene");
+        TableColumn m2teine = new TableColumn("Teine");
+        m1.getColumns().addAll(m1esimene,m1teine);
+        m2.getColumns().addAll(m2esimene,m2teine);
+
+        File file = new File("dice1.png"); // peab välja mõtlema, kuidas ära siduda pildid ja täringute väärtused ja panna need gridpane'i
+        ImageView üks = new ImageView(new Image(file.toURI().toString()));
+
+        GridPane.setConstraints(üks,1,0);
+
+        tableView.getColumns().addAll(reegel,m1,m2);
+
+
+        gridpane.getChildren().addAll(tableView,üks);
+
+        return gridpane;
+    }
     private void Veateade(Alert.AlertType alertType, Window owner, String title, String message) {
         Alert alert = new Alert(alertType);
         alert.setTitle(title);
